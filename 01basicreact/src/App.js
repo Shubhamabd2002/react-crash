7a761ce12ee50c9fa6c153b7c685310ea6e87cc0
SK SHUBHAM <skshubham.abd@gmail.com>
2025-04-21 21:38:45 +0530

    Initial create app with Hello React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      Hello React | learned the classic way of creating a react app using "npx create-react-app [project name]", some basic details about the extra packages that is getting installed.
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>
+      Hello React with vite | SK SHUBHAM
+    </h1>
+  )
+}
+
+export default App
