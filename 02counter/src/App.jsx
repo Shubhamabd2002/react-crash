179d5e1beff46e3d689cb3b371fdf6e0da1f848d
SK SHUBHAM <skshubham.abd@gmail.com>
2025-04-24 23:41:35 +0530

    added a case to update counter multiple times

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,6 +4,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const addMultipleValue = () => {
+    /**
+     * anytime we have to update useState multiple time continuously 
+     * in the bellow case it will create batch(in fiber algorithm) and becuse we are doing same work multiple times it will do it only once
+     */
+    // setCount(count + 1)
+    // setCount(count + 1)
+    // setCount(count + 1)
+    /**
+     * but if we want to update the state multiple times in a row, we can use the functional form of setState.
+     * This way, React will batch the updates and apply them all at once, which is more efficient.
+     * The functional form of setState takes a function as an argument, which receives the previous state as an argument.
+     * This is useful when you need to update the state based on its previous value.
+     *---------------------------------------------------------------------------
+     * if we want same work multiple times we can pass a callback function to it and the state useState will update it multiple times like bellow
+     * setCount((prevCount) => prevCount + 1)
+     */
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+  }
   const addValue = () => {
     setCount(count + 1)
   }
@@ -16,6 +37,7 @@ function App() {
       <h2>Counter value: {count}</h2>
 
       <button onClick={addValue}>Add Value</button> <br/>
+      <button onClick={addMultipleValue}>Add 3 Value</button> <br/>
       <button onClick={removeValue}>Remove Value</button>
     </>
   )
